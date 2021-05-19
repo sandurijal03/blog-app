0234fcc8dbde1502da55a765afe2cb4f72715bf1
Sandip Rijal <sandurijal03@hotmail.com>
2021-05-20 04:37:58 +0545

    filtering all posts with some query

--- api/src/apis/posts.js
+++ api/src/apis/posts.js
@@ -15,9 +15,22 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
+  const { username, catName } = req.query;
+
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
