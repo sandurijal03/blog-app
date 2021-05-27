1ca295e3e9d7ae563794371d2aea04ea355e09d1
Sandip Rijal <sandurijal03@hotmail.com>
2021-05-28 04:38:59 +0545

    implemented deleting the post from frontend

--- frontend/src/components/singlePost/SinglePost.js
+++ frontend/src/components/singlePost/SinglePost.js
@@ -1,11 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './SinglePost.module.css';
 import { Link, useLocation } from 'react-router-dom';
+import { Context } from '../../context/Context';
 
 const SinglePost = () => {
   const location = useLocation();
   const path = location.pathname.split('/')[2];
   const [post, setPost] = useState({});
+  const { user } = useContext(Context);
+  const PF = 'http://localhost:3001/images/';
+
+  let details = {
+    username: user.username,
+  };
+
+  const handleDelete = async () => {
+    try {
+      await fetch(`/post/${post._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(details),
+      });
+
+      window.location.replace('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     const getPost = async () => {
@@ -26,15 +49,21 @@ const SinglePost = () => {
     <div className={classes.singlePost}>
       <div className={classes.singlePostWrapper}>
         {post.photo && (
-          <img src={post.photo} alt='' className={classes.singlePostImg} />
+          <img src={PF + post.photo} alt='' className={classes.singlePostImg} />
         )}
 
         <h1 className={classes.singlePostTitle}>
           {post.title}
-          <div className={classes.singlePostEdit}>
-            <i className={`${classes.singlePostIcon} far fa-edit`}></i>
-            <i className={`${classes.singlePostIcon} far fa-trash-alt`}></i>
-          </div>
+
+          {post.username === user?.username && (
+            <div className={classes.singlePostEdit}>
+              <i className={`${classes.singlePostIcon} far fa-edit`}></i>
+              <i
+                className={`${classes.singlePostIcon} far fa-trash-alt`}
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
 
         <div className={classes.singlePostInfo}>
