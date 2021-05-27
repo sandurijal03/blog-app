d8432ef2f9a014a4f2f372488f72ee9dbc2f90a6
Sandip Rijal <sandurijal03@hotmail.com>
2021-05-27 19:30:59 +0545

    fetching post from backend and displaying post in homepage

--- frontend/src/components/posts/post/Post.js
+++ frontend/src/components/posts/post/Post.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = ({ img }) => {
+const Post = ({ post }) => {
+  console.log(post);
   return (
     <div className={classes.post}>
-      <img className={classes.postImg} src={img} alt='' />
+      <img
+        className={classes.postImg}
+        src={`https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500`}
+        alt=''
+      />
       <div className={classes.postInfo}>
         <div className={classes.postCats}>
           <span className={classes.postCat}>Musicc</span>
           <span className={classes.postCat}>Life</span>
         </div>
-        <span className={classes.postTitle}>Lorem ipsum dolor sit amet.</span>
+        <span className={classes.postTitle}>{post.title}</span>
         <hr />
-        <span className={classes.postDate}>1 hour ago</span>
+        <span className={classes.postDate}>
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <div className={classes.postDescription}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque assumenda
-        tenetur corporis voluptatum eaque accusantium aliquam non. Dignissimos,
-        fuga labore!
-      </div>
+      <div className={classes.postDescription}>{post.desc}</div>
     </div>
   );
 };
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import classes from './Home.module.css';
 import Header from '../../components/header/Header';
@@ -6,11 +6,28 @@ import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch('/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const res = await response.json();
+
+      setPosts(res);
+    };
+    fetchPost();
+  }, []);
+
   return (
     <>
       <Header />
       <div className={classes.home}>
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
